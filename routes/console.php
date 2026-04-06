61e05222410433e767300cd08b20de0dfa367c3e
Ivan Mitev <ivanmitev4555@abv.bg>
2026-04-06 22:41:41 +0300

    Remove duplicate schedule from console.php

--- routes/console.php
+++ routes/console.php
@@ -2,26 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-/*
-|--------------------------------------------------------------------------
-| Scheduled Commands
-|--------------------------------------------------------------------------
-*/
-
-Schedule::command('prices:dispatch-due')
-    ->name('prices.dispatch_due')
-    ->everyFiveMinutes()
-    ->withoutOverlapping()
-    ->runInBackground();
-
-Schedule::command('price-history:cleanup 90')
-    ->name('price_history.cleanup')
-    ->daily()
-    ->withoutOverlapping()
-    ->runInBackground();
